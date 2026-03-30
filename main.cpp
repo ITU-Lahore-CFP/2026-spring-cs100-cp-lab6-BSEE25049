ac986af51cdfe4e2f32e6e8fd09ea992b9ef0936
BSEE25049 <bsee25049@itu.edu.pk>
2026-03-30 16:12:27 +0500

    BSEE25049 L6

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include "functions.h"
-
+#include <iostream>
+using namespace std;
 
 int main() {
-   
+    int n;
+
+    // Task A - Sum of squares
+    cout << "Enter a number: ";
+    if (!(cin >> n)) return 0;
+    cout << "Sum of squares: " << sumOfSquares(n) << endl;
+
+    // Task B - Factorial
+    cout << "Enter a number: ";
+    if (!(cin >> n)) return 0;
+    cout << "Factorial of the number is " << findFactorial(n) << endl;
+
+    return 0;
 }
